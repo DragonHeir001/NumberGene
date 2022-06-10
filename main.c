dd87718ee99ace250db48e94ec6aef5882c3e732
kali <kali@kali>
2022-06-10 16:03:09 -0500

    Had the computer learn your name.

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-char get_character(int number, int squared_num) {
-    if (number*number == squared_num) {
-        return 'a';
-    }
-    return 'b';
-}
-
 int main() {
-    char var1 = get_character(4, 16);
-    printf("%c\n", var1);
+    printf("Insert_name_below:");
+    char name[10];
+    scanf("%s", &name);
+    printf("Welcome, %s", name);
     return 0;
 }
