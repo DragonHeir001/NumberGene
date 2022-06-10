fffe658ddc1af4343b7e48e660366e3752f3d58d
kali <kali@kali>
2022-06-09 21:52:40 -0500

    re-do 2nd challenge

--- main.c
+++ main.c
@@ -1,84 +1,50 @@
 #include <stdio.h>
-void a() {
-    printf("1 a\n");
-}
-void b() {
-    printf("2 b\n");
-}
-void c() {
-    printf("3 a\n");
-}
-void d() {
-    printf("4 b\n");
-}
-void e() {
-    printf("5 a\n");
-}
-void f() {
-    printf("6 b\n");
-}
-void g() {
-    printf("7 a\n");
-}
-void h() {
-    printf("8 b\n");
-}
-void i() {
-    printf("9 a\n");
-}
-void j() {
-    printf("10 b\n");
-}
-void k() {
-    printf("11 a\n");
-}
-void l() {
-    printf("12 b\n");
-}
-void m() {
-    printf("13 a\n");
-}
-void n() {
-    printf("14 b\n");
-}
-void o() {
-    printf("15 a\n");
-}
-void p() {
-    printf("16 b\n");
-}
-void q() {
-    printf("17 a\n");
-}
-void r() {
-    printf("18 b\n");
-}
-void s() {
-    printf("19 a\n");
-}
-void t() {
-    printf("20 b\n");
+void challenge(int line, char c) {
+    printf("%d %c\n", line, c);
 }
 int main() {
-    a();
-    b();
-    c();
-    d();
-    e();
-    f();
-    g();
-    h();
-    i();
-    j();
-    k();
-    l();
-    m();
-    n();
-    o();
-    p();
-    q();
-    r();
-    s();
-    t();
+    int n = 1;
+    char a = 'a';
+    char b = 'b';
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
+    challenge(n, a);
+    n = n + 1;
+    challenge(n, b);
+    n = n + 1;
     return 0;
 }
