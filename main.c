9a78a984a1dd477cdde762494a22760dceae684f
DragonHeir001 <tba25c@gmail.com>
2022-06-10 16:55:43 -0500

    converted fahrenheit to kelvin

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-
+double convert_to_celsius(double f) {
+    f = f - 32; 
+    f = f * 5/9; 
+    return f;
+}
+double convert_to_kelvin(double c) {
+    c = c + 273.15;
+    return c;
+}
 int main() {
-    printf("Insert_name_below:");
-    char name[10];
-    scanf("%s", &name);
-    printf("Welcome, %s", name);
+    printf("Provide fahrenheit temp:");
+    double temp;
+    scanf("%lf", &temp);
+    temp = convert_to_celsius(temp);
+    temp = convert_to_kelvin(temp);
+    printf("Kelvin temp:%lf", temp);
     return 0;
 }
