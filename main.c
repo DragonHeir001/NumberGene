165af072587acba2b42618db16cf166ff692374a
DragonHeir001 <tba25c@gmail.com>
2022-06-10 18:32:24 -0500

    new update on temp challenge

--- main.c
+++ main.c
@@ -8,21 +8,29 @@ double convert_to_kelvin(double c) {
     c = c + 273.15;
     return c;
 }
+//printf("Provide fahrenheit temp:");
+// double temp;
+// char var;
+// scanf("%lf", &temp);
 int main() {
-    printf("Provide fahrenheit temp:");
     double temp;
     char var;
-    scanf("%lf", &temp);
-    printf("Celsius or Kelvin? (c or k) ");
+    char var1;
+    char var2;
+    printf("Fahrenheit, Celsius, or Kelvin? (f, c, or k)");
     scanf("%s", &var);
-    if (var == 'c') {
-        temp = convert_to_celsius(temp);
-        printf("Celsius temp:%lf", temp);
-    }
-    if (var == 'k') {
-        temp = convert_to_celsius(temp);
-        temp = convert_to_kelvin(temp);
-        printf("Kelvin temp:%lf", temp);
-    } 
+    printf("Convert to Celsius or Kelvin or Fahrenheit? (c, f or k)");
+    scanf("%s", &var1);
+    if (var == 'f') {
+        if (var1 == 'c') {
+            temp = convert_to_celsius(temp);
+            printf("Celsius temp:%lf", temp);
+            if (var == 'k') {
+                temp = convert_to_celsius(temp);
+                temp = convert_to_kelvin(temp);
+                printf("Kelvin temp:%lf", temp);
+            }
+        }
+    }    
     return 0;
 }
