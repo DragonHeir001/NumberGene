fa24a933c0145a541ea1407f1673a2c9d5c120c3
DragonHeir001 <tba25c@gmail.com>
2022-06-11 10:07:46 -0500

    finished the challenge!;

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+double convert_to_celsius1(double k) {
+    k = k - 273.15; 
+    return k;
+}
+double convert_to_fahrenheit1(double c) {
+    c = c * 9/5; 
+    c = c + 32; 
+    return c;
+}
 double convert_to_celsius(double f) {
     f = f - 32; 
     f = f * 5/9; 
@@ -33,25 +42,31 @@ int main() {
         }    
     }    
     if (var == 'c') {
-        if (var == 'f') {
+        if (var1 == 'f') {
+            temp = convert_to_fahrenheit1(temp);
 
         }    
-        if (var == 'c') {
-
+        if (var1 == 'c') {
+            printf("You dumb or something? It's %lf", temp);
         }    
-        if (var == 'k') {
+        if (var1 == 'k') {
+            temp = convert_to_kelvin(temp);
+            printf("Kelvin temp: %lf", temp);
 
         }    
     }    
     if (var == 'k') {
-        if (var == 'f') {
-
+        if (var1 == 'f') {
+            temp = convert_to_celsius1(temp);
+            temp = convert_to_fahrenheit1(temp);
+            printf ("Fahrenheit temp: %lf", temp);
         }    
-        if (var == 'c') {
-
+        if (var1 == 'c') {
+            temp = convert_to_celsius1(temp);
+            printf("Celsius temp: %lf", temp);
         }    
-        if (var == 'k') {
-
+        if (var1 == 'k') {
+            printf("You dumb or something? It's %lf", temp);
         }    
     }    
     return 0;
